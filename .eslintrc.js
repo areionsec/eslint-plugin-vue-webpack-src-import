80cc8b0207cae4baf084336d4342b38eb908d622
hodbn <hodbn@users.noreply.github.com>
2021-01-05 15:23:21 +0200

    Add lint using eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
   env: {
     node: true,
+    es6: true,
   },
+
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+
   extends: ['eslint:recommended'],
 
   plugins: ['prettier'],
